ea9ce7e946a913ab44921c9f57d2c9838fa0da80
kipngeno-isaac <75642106+kipngeno-isaac@users.noreply.github.com>
2021-05-13 22:09:51 +0300

    added fab btn and dashboard

--- src/views/layouts/Application.vue
+++ src/views/layouts/Application.vue
@@ -1,15 +1,69 @@
 <template>
   <div class="app--full-height">
     <div class="app--main-content">
-      <v-container class="app--content-container" fluid>
+    <v-app-bar
+      color="deep-purple"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>  
+    <v-container class="app--content-container" fluid>
         <router-view />
       </v-container>
     </div>
+    <v-fab-transition>
+              <v-btn
+                v-show="!hidden"
+                color="pink"
+                dark
+                absolute
+                bottom
+                right
+                fab
+              >
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-fab-transition>
   </div>
 </template>
 
 <script>
 export default {
-
+ data: () => ({
+      drawer: false,
+      group: null,
+    }),
 }
 </script>
